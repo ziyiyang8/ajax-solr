6c14340264682b65ab6f6ad67970c1b0aabc5cb4
Ziyi <Ziyi@DESKTOP-NVVBMNN>
2016-05-08 18:13:41 -0700

    phone search

---
+++ examples/coen272_project2/js/reuters.js
@@ -0,0 +1,12 @@
+var Manager;
+
+(function ($) {
+	
+  $(function () {
+    Manager = new AjaxSolr.Manager({
+      solrUrl: 'http://evolvingweb.ca/solr/reuters/'
+    });
+    Manager.init();
+  });
+  
+})(jQuery);
